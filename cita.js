fdef75351f75d84ad4986a5283e5bc578dc50a2a
gabiPer <gabriela.r.peralta@gmail.com>
2024-01-08 19:52:12 -0300

    Inicio de proyecto

---
+++ cita.js
@@ -0,0 +1,102 @@
+let citas = [
+    {
+      'autor': 'Albert Einstein',
+      'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
+    },
+    {
+      'autor': 'Anónimo',
+      'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
+    },
+    {
+      'autor': 'Alan Kay',
+      'texto': 'La mejor forma de predecir el futuro es inventarlo.'
+    },
+    {
+      'autor': 'Amelia Earhart',
+      'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
+    },
+    {
+      'autor': 'Aristóteles',
+      'texto': 'La calidad no es un acto, es un hábito.'
+    },
+    {
+      'autor': 'Benjamin Franklin',
+      'texto': 'Dímelo y lo olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé.'
+    },
+    {
+      'autor': 'Charles R. Swindoll',
+      'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
+    },
+    {
+      'autor': 'Jane Goodall',
+      'texto': 'Lo que haces, hace una diferencia. Y debes decidir qué tipo de diferencia quieres hacer.'
+    },
+    {
+      'autor': 'John Muir',
+      'texto': 'El poder de la imaginación nos hace infinitos.'
+    },
+    {
+      'autor': 'Mark Twain',
+      'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
+    },
+    {
+     'autor':'Dante Alighieri',
+     'texto':'No fuisteis criados para vivir como bestias, sino para seguir en pos de la virtud y la sabiduría', 
+    },
+    {
+      'autor':'Ella Fitzgerald',
+      'texto':'No se trata de donde vienes, sino de donde vas', 
+     },
+     {
+      'autor':'Albert Einstein',
+      'texto':'Nunca consideres el estudio como una obligación, sino como una oportunidad para penetrar en el bello y maravilloso mundo del saber', 
+     },
+     {
+      'autor':'Proverbio hindú',
+      'texto':'Con mis maestros he aprendido mucho; con mis colegas, más; con mis alumnos todavía más', 
+     },
+     {
+      'autor':'Edward Benjamin Britten',
+      'texto':'Aprender es como remar contra corriente: en cuanto se deja, se retrocede', 
+     },
+     {
+      'autor':'Confucio',
+      'texto':'Me lo contaron y lo olvidé; lo vi y lo entendí; lo hice y lo aprendí', 
+     },
+     {
+      'autor':'Aristóteles',
+      'texto':'La inteligencia consiste no solo en el conocimiento, sino también en la destreza de aplicar los conocimientos en la práctica', 
+     },
+     {
+      'autor':' Confucio',
+      'texto':'Aprender sin reflexionar es una ocupación inútil', 
+     },
+     {
+      'autor':'Ralph Waldo Emerson',
+      'texto':'Todo hombre que conozco es superior a mí en algún sentido. En ese sentido, aprendo de él', 
+     },
+     {
+      'autor':'Cicerón',
+      'texto':'Si quieres aprender, enseña', 
+     },
+     {
+      'autor':'René Descartes',
+      'texto':'Lo poco que he aprendido carece de valor, comparado con lo que ignoro y no desespero en aprender', 
+     },
+     {
+      'autor':'Mahatma Gandhi',
+      'texto':'Vive como si fueses a morir mañana. Aprende como si fueses a vivir siempre', 
+     },
+     {
+      'autor':'Margaret Fuller',
+      'texto':'Hoy un lector, mañana un líder', 
+     },
+     {
+      'autor':'Nora Roberts',
+      'texto':'Si no persigues lo que quieres, nunca lo tendrás. Si no vas hacia delante, siempre estarás en el mismo lugar', 
+     },
+     {
+      'autor':'Proverbio chino',
+      'texto':'El viaje es la recompensa',
+     }
+  ];
